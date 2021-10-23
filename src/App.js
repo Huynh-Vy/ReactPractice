c3b214e7d0048edf2a4cc88a164a70df0d69c3aa
Vy Huynh <havy271@gmail.com>
2021-10-23 09:34:30 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import ToDoFeature from './features/ToDo';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoFeature />
+
+    </div>
+
+
+  );
+}
+
+export default App;
